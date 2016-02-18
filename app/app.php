4d9a843666f491fca595f52d180c9c8fc2b1066e
Joseph Karasek and Marika Allely <mballely@gmail.com>
2016-02-18 08:49:25 -0800

    build simple UI with form and message output

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Scrabble.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -8,7 +9,20 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
-            // twig input associative array
+            'form' => true
+        ));
+    });
+
+    $app->get("/score", function() use ($app) {
+        $my_Scrabble = new Scrabble;
+        $result = $my_Scrabble->returnScore($_GET['word']);
+
+        return $app['twig']->render('index.html.twig', array(
+            'form' => true,
+            'message' => array(
+                'type' => 'info',
+                'text' => $_GET['word'] . " is worth " . $result . " points."
+            )
         ));
     });
 
